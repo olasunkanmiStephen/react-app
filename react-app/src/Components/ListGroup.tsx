041df8a6cbf6e745d905d8b899ec8e51fe21fffe
Stephen Olasunkanmi <bonniedevis727@gmail.com>
2023-11-28 07:06:25 +0100

    using jsx function and operations

--- react-app/src/Components/ListGroup.tsx
+++ react-app/src/Components/ListGroup.tsx
@@ -1,20 +1,21 @@
 function ListGroup() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+    items = [];
 
-    const items = [
-        'New York',
-        'San Francisco',
-        'Tokyo',
-        'London',
-        'Paris'
-    ]
-
-    return <>
-        <h1>List Group</h1>
+    const getMessage = () => {
+        return items.length === 0 && <p>No items found</p>;
+    }
+  return (
+    <>
+        <h1>List</h1>
+        {getMessage()}
         <ul className="list-group">
-            
-            {items.map(item => <li> {item} </li>)}
-        </ul>
+        {items.map((item) => (
+          <li key={item}> {item} </li>
+        ))}
+      </ul>
     </>
+  );
 }
 
 export default ListGroup;
