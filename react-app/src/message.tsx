f981d756f690f0e3374317b3288c11be8ba66642
Stephen Olasunkanmi <bonniedevis727@gmail.com>
2023-11-28 06:20:21 +0100

    js variable in react

--- react-app/src/message.tsx
+++ react-app/src/message.tsx
@@ -1,8 +1,10 @@
 function Message() {
 
 
-    const name = 'Steph'
-    return <h1>Hello {name}</h1>;
-}
+    const name = '';
+        if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+    }
 
 export default Message;
