4e2cc348818f9cedc5962e5f9f31c2ca2e0c08e9
Stephen Olasunkanmi <bonniedevis727@gmail.com>
2023-11-28 15:50:06 +0100

    complete react course

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Alert from "./Components/alert";
+import { useState } from "react";
 import Button from "./Components/Button"; // Use PascalCase for component names
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
+
+      {alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
       {/* Using the Button component */}
-      <Button onClick={() => console.log('clicked')}>My Button</Button>
+      <Button color="danger" onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
--- react-app/src/Components/button.tsx
+++ react-app/src/Components/button.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
 interface Props {
+  children: string;
+  color?: 'danger' | 'primary' | 'secondary';
   onClick: () => void;
 }
 
-const Button: React.FC<Props> = ({ children, onClick }) => {
+const Button: React.FC<Props> = ({ children, onClick, color = 'danger' }) => {
   return (
-    <button type="button" className="btn btn-primary" onClick={onClick}>
+    <button type="button" className={"btn btn-" + color} onClick={onClick}>
       {children}
     </button>
   );
 };
 
 export default Button;
-
