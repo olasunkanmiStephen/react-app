553dc30c024847d07647605dfc2dbcea8f70895b
Stephen Olasunkanmi <bonniedevis727@gmail.com>
2023-11-28 10:48:00 +0100

    my exercise

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,11 @@
 import Alert from "./Components/alert";
+import button from "./Components/button";
 
 function App() {
   return (
     <div>
       <Alert>Hello <span>World</span></Alert>
+      <button> hello</button>
     </div>
   );
 }
